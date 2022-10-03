a038adf3acc9969b1e75af1d8a80f8aae9554d21
Tpinion <657750019@qq.com>
2022-10-03 22:18:30 +0800

    rpa for artifacts

---
+++ genshin_mummy/artifact_helper/lock_and_unlock.py
@@ -0,0 +1,237 @@
+from typing import Optional
+
+import cv2
+import numpy as np
+import pyautogui
+from genshin_mummy.artifact_helper.type import (Artifact, ArtifactPage,
+                                                ArtifactType, EntryType)
+from genshin_mummy.ocr.opt import are_text_chunks_aligned_vertically
+from genshin_mummy.ocr.type import (Alignment, TextChunk, TextChunkCollection,
+                                    build_text_chunks_from_paddle_ocr,)
+
+from genshin_mummy.type import Box, Direction, Point
+from paddleocr import PaddleOCR
+from PIL import Image as PILImage
+
+# TODO: 视PADDLE OCR结果可能要归一化
+SPACE_CHAR = ' '
+STAR_CHAR = '★'
+PLUS_CHAR = '+'
+DOT_CHAR = '·'
+PERCENT_CHAR = '%'
+SUBENTRY_PATTERN = f'{DOT_CHAR}.*?\{PLUS_CHAR}[\d.]*[{PERCENT_CHAR}]?'
+
+
+def get_entry_type(entry_key: str, entry_value: str):
+    entry_type = None
+    if entry_value.endswith(PERCENT_CHAR):
+        if entry_key == EntryType.HP.value:
+            entry_type = EntryType.HP_PERCENTAGE
+        if entry_key == EntryType.ATK.value:
+            entry_type = EntryType.ATK_PERCENTAGE
+        if entry_key == EntryType.DEF.value:
+            entry_type = EntryType.DEF_PERCENTAGE
+
+    if entry_type is None:
+        for cand_entry_type in EntryType:
+            if entry_key == cand_entry_type.value:
+                entry_type = cand_entry_type
+                break
+    return entry_type
+
+
+def recognize_artifact_informations(ocr, screen: np.ndarray):
+    # TODO: 移到ArtifactDescription里去
+
+    # 移除显著游离余左对齐的文本，规避OCR噪声字符
+    sigma = 3
+    ocr_items = ocr.ocr(screen, cls=False)
+    text_chunks = build_text_chunks_from_paddle_ocr(ocr_items)
+    lefts = [tc.left for tc in text_chunks]
+    left_std = np.std(lefts)
+    left_mean = np.mean(lefts)
+    valid_text_chunks = []
+    for chunk in text_chunks:
+        if abs(chunk.left - left_mean) > sigma * left_std:
+            continue
+        valid_text_chunks.append(chunk)
+    if not valid_text_chunks:
+        cv2.imshow('img', screen)
+        cv2.waitKey(0)
+        breakpoint()
+    chunk_clct = TextChunkCollection(valid_text_chunks)
+
+    _chunks = []
+    for artifact_type in ArtifactType:
+        _chunks = chunk_clct.find(artifact_type.value, conf=0.65)
+        break
+    if _chunks:
+        type_chunk = _chunks[0]
+    else:
+        type_chunk = chunk_clct.find_by_index(direction=Direction.VERT, idx=1)
+
+    name_chunk = type_chunk.top_text_chunk
+
+    entry_chunk = type_chunk.bottom_text_chunk
+    entry_value_chunk = entry_chunk.bottom_text_chunk
+    stars_chunk = entry_value_chunk.bottom_text_chunk
+
+    if not stars_chunk.text.startswith(STAR_CHAR):
+        _chunks = chunk_clct.find_startswith(STAR_CHAR)
+        stars_chunk = _chunks[0]
+
+    level_chunk = stars_chunk.bottom_text_chunk
+    if level_chunk.text.startswith(PLUS_CHAR):
+        _chunks = chunk_clct.find_startswith(PLUS_CHAR)
+        level_chunk = _chunks[0]
+
+    subentry_chunks = []
+    subentry_chunk = level_chunk.bottom_text_chunk
+    while True:
+        if subentry_chunk.text.startswith(DOT_CHAR):
+            subentry_chunks.append(subentry_chunk)
+        else:
+            break
+        subentry_chunk = subentry_chunk.bottom_text_chunk
+
+    if len(subentry_chunks) > 5 or not are_text_chunks_aligned_vertically(
+            subentry_chunks, Alignment.LEFT):
+        subentry_chunks = chunk_clct.find_pattern(SUBENTRY_PATTERN)
+        # TODO: 校验值正确性，若不正确切换模板策略抽取
+
+    entry_type = get_entry_type(entry_chunk.text, entry_value_chunk.text)
+
+    # TODO
+    assert entry_type
+
+    subentries = {}
+    for chunk in subentry_chunks:
+        # TODO: 想办法规避下OCR稳定性影响, 目前好像没问题
+        text = chunk.text.strip(f'{DOT_CHAR}{SPACE_CHAR}')
+        subentry_key, subentry_value = text.split(PLUS_CHAR)
+        subentry_type = get_entry_type(subentry_key, subentry_value)
+        subentries[subentry_type] = subentry_value
+
+    artifact = Artifact(
+        name=name_chunk.text,
+        type=type_chunk.text,
+        entry={entry_chunk.text: entry_value_chunk.text},
+        stars=stars_chunk.text.count(STAR_CHAR),
+        level=int(level_chunk.text.lstrip(PLUS_CHAR)),
+        subentries=subentries,
+    )
+    assert isinstance(level_chunk, TextChunk)
+    return artifact, level_chunk
+
+
+def whether_or_not_to_lock(artifact):
+    # TODO: 抽象出策略类
+    # 算法参考
+    # https://www.bilibili.com/video/BV1sZ4y1e7h8/?spm_id_from=333.1007.top_right_bar_window_history.content.click
+    # https://www.bilibili.com/video/BV1mB4y177a6/?spm_id_from=333.1007.top_right_bar_window_history.content.click
+
+    # 等级大于0=>锁
+    if artifact.level > 0:
+        return True
+
+    # 非五星=>不锁
+    if artifact.stars < 5:
+        return False
+
+    # 沙、杯、帽主词条为类别独有词条=>锁
+    if (artifact.type not in {
+            ArtifactType.FLOWER_OF_LIFE, ArtifactType.PLUME_OF_DEATH
+    } and artifact.entry not in {EntryType.HP, EntryType.ATK, EntryType.DEF}):
+        return True
+
+    # 双暴词条=>锁
+    subentry_types = set(artifact.subentries.keys())
+    if {EntryType.CRIT_DMG, EntryType.CRIT_RATE}.issubset(subentry_types):
+        return True
+
+    # 初始四词条 且不要存在所有小攻防命都有=>锁
+    if len(subentry_types) == 4 and not {
+            EntryType.HP, EntryType.ATK, EntryType.DEF
+    }.issubset(subentry_types):
+        return True
+
+    # 小攻击、小防御、小生命大于等于两个=>不锁
+    if len({EntryType.HP, EntryType.ATK, EntryType.DEF} - subentry_types) >= 2:
+        return False
+
+    return True
+
+
+def locate_lock_icon(
+    top_limit: int,
+    bottom_limit: int,
+    left_limit: int,
+    screen: np.ndarray,
+    desc_loc_mask: np.ndarray,
+):
+    desc_loc_mask[:, :left_limit] = 0
+    desc_loc_mask[:top_limit, :] = 0
+    desc_loc_mask[bottom_limit:, :] = 0
+    gray = cv2.cvtColor(screen, cv2.COLOR_RGB2GRAY)
+    thres = cv2.adaptiveThreshold(
+        src=gray,
+        maxValue=255,
+        adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        thresholdType=cv2.THRESH_BINARY_INV,
+        blockSize=11,
+        C=2,
+    )
+    thres = cv2.bitwise_and(thres, thres, mask=desc_loc_mask)
+    roi_left, roi_top, roi_width, roi_height = cv2.boundingRect(thres)
+    center_point = Box(roi_left, roi_top, roi_width, roi_height)
+    roi = screen[roi_top:roi_top + roi_height, roi_left:roi_left + roi_width]
+    channel_red_roi = roi[:, :, 0]
+    # TODO: 经验值有效但不太保险
+    THRESH = 30
+    if np.mean(channel_red_roi) - np.mean(roi[:, :, 1:]) > THRESH:
+        lock_status = True
+    else:
+        lock_status = False
+    center_point = Point(
+        x=roi_left + roi_width // 2,
+        y=roi_top + roi_height // 2,
+    )
+    return lock_status, center_point
+
+
+def run_pipeline(max_num: int, debug_root: Optional[str] = None):
+    if debug_root:
+        from pathlib import Path
+        debug_root = Path(debug_root)
+    ocr = PaddleOCR(use_angle_cls=False, lang="ch")
+    artifact_page = ArtifactPage()
+    for idx, _ in enumerate(artifact_page.iter_artifacts(max_num)):
+        screen = pyautogui.screenshot()
+        screen = np.asarray(screen)
+        screen = cv2.bitwise_and(screen,
+                                 screen,
+                                 mask=artifact_page.desc_loc_mask)
+        PILImage.fromarray(screen).save(debug_root / f'{idx}_ocr.png')
+        artifact, level_chunk = recognize_artifact_informations(ocr, screen)
+        expect_status = whether_or_not_to_lock(artifact)
+        lock_status, icon_center = locate_lock_icon(
+            top_limit=int(level_chunk.top),
+            bottom_limit=int(level_chunk.bottom),
+            left_limit=int(artifact_page.desc_loc.left +
+                           artifact_page.desc_loc.width // 2),
+            screen=screen,
+            desc_loc_mask=artifact_page.desc_loc_mask.copy(),
+        )
+        if lock_status != expect_status:
+            pyautogui.leftClick(
+                icon_center.x,
+                icon_center.y,
+                duration=artifact_page.mouse_move_time,
+            )
+        artifact_page.move_to_artifact_list()
+
+
+import time
+
+time.sleep(10)
+run_pipeline(60, './debug_folder')
---
+++ genshin_mummy/artifact_helper/type.py
@@ -0,0 +1,402 @@
+import time
+from enum import Enum, unique
+from typing import Dict, List, Optional, Tuple
+
+import attrs
+import cv2
+import numpy as np
+import pyautogui
+from genshin_mummy.ocr.type import TextChunkCollection
+from genshin_mummy.opt import (
+    diff_two_images,
+    ensure_mouse_in_safe_location,
+    locate_roi_location_from_diffs,
+)
+from genshin_mummy.type import Box, Direction, Point
+
+
+@unique
+class EntryType(Enum):
+    HP = '生命值'
+    HP_PERCENTAGE = '生命值百分比'
+    ATK = '攻击力'
+    ATK_PERCENTAGE = '攻击力百分比'
+    DEF = '防御力'
+    DEF_PERCENTAGE = '防御力百分比'
+
+    CRIT_DMG = '暴击伤害'
+    CRIT_RATE = '暴击率'
+
+    ELEMENTAL_MASTERY = '元素精通'
+    ENERGY_RECHARGE = '元素充能'
+
+    PYRO_DMG_BONUS = '火元素伤害加成'
+    HYDRO_DMG_BONUS = '水元素伤害加成'
+    CRYO_DMG_BONUS = '冰元素伤害加成'
+    ELECTRO_DMG_BONUS = '雷元素伤害加成'
+    DENDRO_DMG_BONUS = '草元素伤害加成'
+    ANEMO_DMG_BONUS = '风元素伤害加成'
+    GEO_DMG_BONUS = '岩元素伤害加成'
+    PHYSICAL_DMG_BONUS = '物理伤害加成'
+    HEALING_BONUS = '治疗加成'
+
+
+@unique
+class ArtifactType(Enum):
+    FLOWER_OF_LIFE = '生之花'
+    PLUME_OF_DEATH = '死之羽'
+    SANDS_OF_EON = '时之沙'
+    GOBLETS_OF_EONOTHEM = '空之杯'
+    CERCLETS_OF_LOGOS = '理之冠'
+
+
+@attrs.define
+class Artifact:
+    name: str = attrs.field()
+    type: ArtifactType = attrs.field()
+    entry: Dict[EntryType, str] = attrs.field()
+    stars: int = attrs.field()
+    level: int = attrs.field()
+    subentries: Dict[EntryType, str] = attrs.field()
+
+
+@attrs.define
+class ArtifactDescription:
+    # TODO:
+    image: np.ndarray = attrs.field()
+    text_chunk_collection: TextChunkCollection = attrs.field()
+
+
+@attrs.define
+class ArtifactPage:
+    mouse_move_time: float = attrs.field(default=0.2)
+    rendering_time: float = attrs.field(default=0.5)
+    scroll_steps: int = attrs.field(default=5)
+    first_artifact_loc: Box = attrs.field(default=None)
+    x_offset: Optional[int] = attrs.field(default=None)
+    y_offset: Optional[int] = attrs.field(default=None)
+    loc_iou_thres: float = attrs.field(default=0.1)
+    diff_thres_ratio: float = attrs.field(default=0.1)
+
+    screen_width: int = attrs.field(init=False)
+    screen_height: int = attrs.field(init=False)
+    screen_area: int = attrs.field(init=False)
+
+    desc_loc: Box = attrs.field(init=False)
+    list_loc: Box = attrs.field(init=False)
+    list_loc_mask: np.ndarray = attrs.field(init=False)
+    desc_loc_mask: np.ndarray = attrs.field(init=False)
+
+    rough_list_loc_ratio: Tuple[float, float, float,
+                                float] = attrs.field(default=(0.1, 0.17, 0.6,
+                                                              0.7))
+    rough_list_loc: Box = attrs.field(init=False)
+    rough_desc_loc_ratio: Tuple[float, float, float,
+                                float] = attrs.field(default=(0.68, 0.17, 0.26,
+                                                              0.7))
+    rough_desc_loc: Box = attrs.field(init=False)
+
+    col_points: List[int] = attrs.field(init=False, factory=list)
+
+    def __attrs_post_init__(self):
+        # 需处于切换圣遗物页签后的初始状态
+        self.screen_width, self.screen_height = pyautogui.size()
+        self.screen_area = self.screen_height * self.screen_width
+        self.rough_desc_loc = Box(
+            left=self.screen_width * self.rough_desc_loc_ratio[0],
+            top=self.screen_height * self.rough_desc_loc_ratio[1],
+            width=self.screen_width * self.rough_desc_loc_ratio[2],
+            height=self.screen_height * self.rough_desc_loc_ratio[3])
+        self.rough_list_loc = Box(
+            left=self.screen_width * self.rough_list_loc_ratio[0],
+            top=self.screen_height * self.rough_list_loc_ratio[1],
+            width=self.screen_width * self.rough_list_loc_ratio[2],
+            height=self.screen_height * self.rough_list_loc_ratio[3])
+        self.locate_artifact_list()
+        self.locate_artifact_description()
+        # TODO: 双向校验圣遗物列表区域和描述区域的坐标位置准确性
+        self.first_artifact_loc = self.locate_selected_artifact()
+        self.locate_artifact_rows_and_columns()
+
+    def is_same_artifact(self, loc_a: Box, loc_b: Box):
+        iou = loc_a.overlap(loc_b)
+        return iou > self.loc_iou_thres
+
+    def locate_artifact_rows_and_columns(self):
+        down_artifact_loc = self.locate_next_artifact(
+            self.first_artifact_loc,
+            direction=Direction.DOWN,
+        )
+        if down_artifact_loc:
+            self.y_offset = down_artifact_loc.center_y - self.first_artifact_loc.center_y
+        else:
+            # TODO
+            pass
+
+        prev_loc = self.first_artifact_loc
+        self.col_points.append(prev_loc.center_x)
+        x_offsets = []
+        while prev_loc:
+            next_loc = self.locate_next_artifact(
+                prev_loc,
+                direction=Direction.RIGHT,
+            )
+            if next_loc:
+                self.col_points.append(next_loc.center_x)
+                x_offsets.append(next_loc.center_x - prev_loc.center_x)
+            prev_loc = next_loc
+        if x_offsets:
+            self.x_offset = int(sum(x_offsets) / len(x_offsets))
+        else:
+            # TODO
+            pass
+
+    def scroll(
+        self,
+        direction: Direction,
+        clicks: int = 1,
+        times: int = 1,
+    ):
+        if direction == Direction.UP:
+            clicks = abs(clicks)
+        elif direction == Direction.DOWN:
+            clicks = -abs(clicks)
+        else:
+            raise NotImplementedError()
+        for _ in range(self.scroll_steps * times):
+            pyautogui.scroll(clicks)
+        self.wait_rendering()
+
+    def move_to_artifact_list(self):
+        if getattr(self, 'list_loc', None):
+            ensure_mouse_in_safe_location(
+                loc=self.list_loc,
+                duration=self.mouse_move_time,
+            )
+        else:
+            ensure_mouse_in_safe_location(loc=self.rough_list_loc,
+                                          duration=self.mouse_move_time,
+                                          use_box_center=True)
+
+    def _scroll_artifact_list(
+        self,
+        direction: Direction,
+        times: int = 5,
+        only_scrolling: bool = True,
+    ):
+        self.move_to_artifact_list()
+        if only_scrolling:
+            self.scroll(direction=direction, clicks=1, times=times)
+            self.wait_rendering()
+            return False
+        prev_screen = pyautogui.screenshot()
+        self.scroll(direction=direction, clicks=1, times=times)
+        self.wait_rendering()
+        next_screen = pyautogui.screenshot()
+        diff = diff_two_images(prev_screen, next_screen)
+        diff_num = np.count_nonzero(diff)
+        return diff_num / self.screen_area < self.diff_thres_ratio
+
+    def scroll_artifact_list(
+        self,
+        direction: Direction,
+        times: int = 5,
+        until_boundary: bool = False,
+        only_scrolling: bool = True,
+    ):
+        if until_boundary:
+            only_scrolling = False
+            reach_boundary = False
+            while not reach_boundary:
+                reach_boundary = self._scroll_artifact_list(
+                    direction=direction,
+                    times=times,
+                    only_scrolling=only_scrolling,
+                )
+                # TODO: if timeout return false
+            return True
+        else:
+            reach_boundary = self._scroll_artifact_list(
+                direction=direction,
+                times=times,
+                only_scrolling=only_scrolling,
+            )
+            return reach_boundary
+
+    def iter_artifacts(self, max_num: Optional[int] = None):
+        count = 0
+        self.scroll_artifact_list(
+            direction=Direction.UP,
+            times=1,
+            until_boundary=True,
+        )
+
+        row_head_loc = self.first_artifact_loc
+        excced_max_num = False
+        while True:
+            for x in self.col_points:
+                count += 1
+                if max_num and count > max_num:
+                    excced_max_num = True
+                    break
+                pyautogui.leftClick(x=x, y=row_head_loc.center_y)
+                yield
+
+            if excced_max_num:
+                break
+
+            loc = self.locate_selected_artifact()
+
+            if loc.center_y + self.y_offset < self.list_loc.bottom:
+                row_head_loc = self.locate_aim_artifact_based_on_point(
+                    aim_x=self.col_points[0],
+                    aim_y=loc.center_y + self.y_offset,
+                )
+            else:
+                row_head_loc = None
+
+            reach_end = False
+            while row_head_loc is None:
+                reach_end = self.scroll_artifact_list(
+                    direction=Direction.DOWN,
+                    only_scrolling=False,
+                )
+                if reach_end:
+                    break
+                loc = self.locate_selected_artifact()
+                row_head_loc = self.locate_aim_artifact_based_on_point(
+                    aim_x=self.col_points[0],
+                    aim_y=loc.center_y + self.y_offset,
+                )
+            if reach_end:
+                break
+
+    def wait_rendering(self, count: int = 1):
+        time.sleep(count * self.rendering_time)
+
+    def generate_roi_mask(self, roi: Box):
+        mask = np.zeros(
+            shape=(self.screen_height, self.screen_width),
+            dtype=np.uint8,
+        )
+        mask[roi.top:roi.bottom, roi.left:roi.right] = 255
+        return mask
+
+    def locate_artifact_description(self):
+        ensure_mouse_in_safe_location(
+            loc=self.rough_desc_loc,
+            use_box_center=True,
+        )
+        clicks = 5
+        prev_screen = pyautogui.screenshot()
+        self.scroll(direction=Direction.DOWN, clicks=clicks)
+        after_screen = pyautogui.screenshot()
+        self.scroll(direction=Direction.UP, clicks=2 * clicks)
+        diffs = diff_two_images(prev_screen, after_screen)
+        # 左半侧存在选中圣遗物的闪烁区域，不属于ROI
+        diffs[:, :diffs.shape[1] // 2] = 0
+
+        roi = locate_roi_location_from_diffs(
+            diffs,
+            self.diff_thres_ratio,
+        )
+        if roi is None:
+            # TODO:
+            pass
+        self.desc_loc = roi
+        self.desc_loc_mask = self.generate_roi_mask(roi)
+
+    def locate_artifact_list(self):
+        self.move_to_artifact_list()
+        prev_screen = pyautogui.screenshot()
+        reach_end = self.scroll_artifact_list(
+            direction=Direction.DOWN,
+            times=1,
+            only_scrolling=False,
+        )
+        after_screen = pyautogui.screenshot()
+        self.scroll_artifact_list(direction=Direction.UP, times=2)
+        diffs = diff_two_images(prev_screen, after_screen)
+
+        roi = locate_roi_location_from_diffs(diffs, self.diff_thres_ratio)
+
+        if roi and not reach_end:
+            self.list_loc = roi
+        elif roi and reach_end:
+            # TODO:
+            raise NotImplementedError()
+        else:
+            # TODO:
+            raise NotImplementedError()
+
+        self.list_loc_mask = self.generate_roi_mask(roi)
+
+    def locate_selected_artifact(self):
+        # 基于被选中圣遗物有闪烁效果，获取选中圣遗物外边框
+        prev_screen = pyautogui.screenshot()
+        prev_screen = np.asarray(prev_screen)
+        after_screen = pyautogui.screenshot()
+        after_screen = np.asarray(after_screen)
+        diffs = diff_two_images(prev_screen, after_screen)
+        diffs = cv2.bitwise_and(diffs, diffs, mask=self.list_loc_mask)
+        diffs = cv2.morphologyEx(diffs, cv2.MORPH_OPEN, kernel=(3, 3))
+        x, y, w, h = cv2.boundingRect(diffs)
+        # TODO: resolve find noting
+        artifact_loc = Box(left=x, top=y, width=w, height=h)
+        return artifact_loc
+
+    def locate_aim_artifact_based_on_point(
+        self,
+        aim_x: int,
+        aim_y: int,
+    ):
+        aim_point = Point(x=aim_x, y=aim_y)
+        pyautogui.leftClick(aim_point.x, aim_point.y)
+        selected_loc = self.locate_selected_artifact()
+        if selected_loc.contain(aim_point):
+            return selected_loc
+        return None
+
+    def locate_next_artifact(
+        self,
+        basic_artifact_loc: Box,
+        direction: Direction,
+    ):
+        if self.x_offset is None:
+            x_offset = basic_artifact_loc.width
+        else:
+            x_offset = self.x_offset
+        if self.y_offset is None:
+            y_offset = basic_artifact_loc.height
+        else:
+            y_offset = self.y_offset
+
+        aim_x = basic_artifact_loc.center_x
+        aim_y = basic_artifact_loc.center_y
+        if direction == Direction.LEFT:
+            aim_x = max(aim_x - x_offset, self.list_loc.left)
+        elif direction == Direction.RIGHT:
+            aim_x = min(aim_x + x_offset, self.list_loc.right)
+        elif direction == Direction.UP:
+            aim_y = max(aim_y - y_offset, self.list_loc.top)
+        elif direction == Direction.DOWN:
+            aim_y = min(aim_y + y_offset, self.list_loc.bottom)
+        else:
+            raise NotImplementedError()
+
+        pyautogui.leftClick(aim_x, aim_y)
+        next_artifact_loc = self.locate_selected_artifact()
+        if not self.is_same_artifact(
+                loc_a=basic_artifact_loc,
+                loc_b=next_artifact_loc,
+        ):
+            return next_artifact_loc
+        return None
+
+
+if __name__ == '__main__':
+    import time
+    time.sleep(5)
+    artifact_page = ArtifactPage()
+    for _ in artifact_page.iter_artifacts(max_num=10):
+        pass
